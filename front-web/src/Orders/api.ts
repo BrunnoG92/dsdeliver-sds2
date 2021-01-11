f981ac88ef508b8e1fffd2e8696bd2d6ec47c0ae
Bruno <BrunoG96@outlook.com>
2021-01-11 01:12:12 -0300

    Add env vars

--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { OrderPayload } from "./types";
-const API_URL = "http://localhost:"
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 export function fetchProducts (){
-    return axios (`${API_URL}8080/products`)
+    return axios (`${API_URL}/products`)
 }
 export function fetchLocalMapBox (local: string) {
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}
