90cd32b06b6141f1ceaaf343210a177a6f80bc47
Bruno <BrunoG96@outlook.com>
2021-01-16 00:22:44 -0300

    back folder mobile-front

---
+++ dsdeliver/front-mobile/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useFonts, OpenSans_400Regular,OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/Routes';
+export default function App() {
+
+
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+
+
+  return (
+    <View style={styles.container}>
+     
+     <Routes/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+   
+  },
+});
---
+++ dsdeliver/front-mobile/src/Orders/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+function Orders() {
+    const handelOnPress = () => {
+
+    }
+    return (
+        <>
+            <View>
+              <Text> Listagem de Pedidos </Text>
+            </View>
+        </>
+
+    );
+}
+const styles = StyleSheet.create({
+
+});
+export default Orders;
